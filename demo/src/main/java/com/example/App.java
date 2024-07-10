0119c5b65f02124ea2dda0f03ca230b8aad42bb3
alfonsoarodriguez1975 <alfonso.a.rodriguez@gmail.com>
2024-07-10 11:07:08 +0000

    objeto foo

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -1,13 +1,32 @@
 package com.example;
 
+import java.util.UUID;
+
+import com.example.entity.Foo;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public static void setupHibernate(){
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+        .build();
+        SessionFactory sessionFactory = new MetadataSources(registry)          
+        .addAnnotatedClass(Foo.class)
+        .buildMetadata()
+        .buildSessionFactory();
+        Session session = sessionFactory.openSession();          
+        var tr = session.beginTransaction();
+        Foo foo = new Foo();
+        foo.id = UUID.randomUUID();          
+        foo.name = "aaaa";
+        session.persist(foo);
+        tr.commit();                 
+        session.close();
     }
 }
---
+++ demo/src/main/java/com/example/entity/Foo.java
@@ -0,0 +1,13 @@
+package com.example.entity;
+
+/**
+ * Hello world!
+ *
+ */
+@Entity
+@Table(name="foo")
+public class Foo 
+{
+    public UUID id;
+    public String name;
+}
